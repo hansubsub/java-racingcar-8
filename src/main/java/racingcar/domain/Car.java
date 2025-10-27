279fe945c653051a1a3fcfe5dc706ae1c45250c1
subin <han04245@naver.com>
2025-10-27 16:28:08 +0900

    feat: Car 클래스 구현
    - 이름 유효성 검사 및 이동 기능

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+public class Car {
+    private static final String ERROR_CAR_NAME_TOO_LONG = "자동차 이름은 5자 이하여야 합니다.";
+    private static final String ERROR_CAR_NAME_NO_SPACE = "자동차 이름에 공백을 포함할 수 없습니다.";
+    private static final String ERROR_CAR_NAME_NO_EMPTY = "자동차 이름으로 빈 값을 입력할 수 없습니다.";
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String carName){
+        validate(carName);
+        this.name = carName;
+    }
+
+    public void move(){
+        position +=1;
+    }
+
+    private void validate(String carName) {
+        if(carName.length()>5){
+            throw new IllegalArgumentException(ERROR_CAR_NAME_TOO_LONG);
+        }
+        if (carName.contains(" ")) {
+            throw new IllegalArgumentException(ERROR_CAR_NAME_NO_SPACE);
+        }
+        if (carName.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_CAR_NAME_NO_EMPTY);
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getPosition(){
+        return position;
+    }
+}
